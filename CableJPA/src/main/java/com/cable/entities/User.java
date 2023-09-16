d7e73d1ac31a1a45f7817401e481b51b7bd5da79
Edwin <Flattoedwin@gmail.com>
2023-09-16 11:11:25 -0400

    fixed setup

--- CableJPA/src/main/java/com/cable/entities/User.java
+++ CableJPA/src/main/java/com/cable/entities/User.java
@@ -1,21 +1,5 @@
 package com.cable.entities;
 
-import java.time.LocalDateTime;
-
-import javax.persistence.*;
-
-@Entity
 public class User {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int id;
-
-	private String username;
-
-	private String password;
-
-	private String avatar;
-	
-	private LocalDateTime timestamp;
 
 }

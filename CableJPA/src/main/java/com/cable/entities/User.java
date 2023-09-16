446e9465498c5f335131fe42cb4c79b0dc586b98
Kevin Puig <119972216+k-puig@users.noreply.github.com>
2023-09-16 02:51:40 -0400

    Created User, Message, and TextChannel Entities

--- CableJPA/src/main/java/com/cable/entities/User.java
+++ CableJPA/src/main/java/com/cable/entities/User.java
@@ -1,5 +1,21 @@
 package com.cable.entities;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.*;
+
+@Entity
 public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String username;
+
+	private String password;
+
+	private String avatar;
+	
+	private LocalDateTime timestamp;
 
 }

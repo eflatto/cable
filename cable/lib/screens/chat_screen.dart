7a760c1e61ca9bb6f84f351075e73b4658c0e5d6
YashTike <yashtike.usa@gmail.com>
2023-09-16 19:28:32 -0400

    Added authentication routes.

--- cable/lib/screens/chat_screen.dart
+++ cable/lib/screens/chat_screen.dart
@@ -3,12 +3,14 @@ import 'package:cable/widgets/chat_messages.dart';
 import 'package:cable/widgets/new_message.dart';
 
 class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blueGrey.shade900,
       appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 38, 241, 166),
+        backgroundColor: const Color.fromARGB(255, 38, 241, 166),
         title: const Text('Cable'), //beautify, different font and style
         actions: [
           IconButton(

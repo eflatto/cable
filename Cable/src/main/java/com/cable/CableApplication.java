e28e39902ba18d36097efa46a999212a10f4d820
Edwin <Flattoedwin@gmail.com>
2023-09-16 02:13:29 -0400

    added spring project

---
+++ Cable/src/main/java/com/cable/CableApplication.java
@@ -0,0 +1,13 @@
+package com.cable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CableApplication.class, args);
+	}
+
+}

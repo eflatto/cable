4943c9546230f8c7c948549c770f5910acbb9d8e
eflatto <119378258+eflatto@users.noreply.github.com>
2023-09-17 10:56:58 -0400

    Delete models directory

--- models/channel.dart
+++
@@ -1,8 +0,0 @@
-import 'package:cable/models/message.dart';
-
-class Channel {
-  const Channel({required this.channelName, required this.messages});
-
-  final String channelName;
-  final List<Message> messages;
-}
--- models/message.dart
+++
@@ -1,6 +0,0 @@
-class Message {
-  const Message({required this.content, required this.user});
-
-  final String content;
-  final String user;
-}
